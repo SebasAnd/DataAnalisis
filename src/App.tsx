40386753e90987187a61d8e0a8a3664cdbc3cad0
Andrés Alejandro Cortés Poveda <andres.alejandro.cortes@correounivalle.edu.co>
2023-11-26 00:44:07 -0500

    cahnge some text values

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,4 @@
-import React, {
-  JSXElementConstructor,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
-  useState,
-} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./App.css";
 import { useFilePicker } from "use-file-picker";
 import { read } from "xlsx";
@@ -31,10 +23,10 @@ type Runner = {
 };
 
 function App() {
-  const { openFilePicker, filesContent, loading } = useFilePicker({
+  const { loading } = useFilePicker({
     accept: ".xlsx",
   });
-  const [fileContentTable, setContentTable] = useState(<>Hello world</>);
+  const [fileContentTable, setContentTable] = useState(<>Select a File</>);
 
   let averageData: RowFormat;
 
@@ -317,31 +309,6 @@ function App() {
       <div>{fileContentTable}</div>
     </div>
   );
-  /*
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )*/
 }
 
 export default App;
